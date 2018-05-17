5c4d8c409da6b5c481f7782ac7b9b5d10006f995
zc <zc@Felix-MBP.lan>
2018-05-17 18:33:44 +0800

    update

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Button } from 'antd';
 import './home.less'
 class Home extends Component {
     // constructor (props) {
@@ -8,6 +9,7 @@ class Home extends Component {
         return (
             <div className="home">
                 <h2>首页</h2>
+                <Button type="primary">Primary</Button>
                 <p>这是一个 react 学习的基本操作的小栗子</p>
                 <p>通过本次学习可以清楚的掌握， react-router、redux的基本使用方法</p>
             </div>
